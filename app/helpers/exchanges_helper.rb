af6f576ddeeafc2ae75473ce25c008bfb885c21f
Vanildo Prates <vprates@gmail.com>
2019-01-17 18:11:46 -0200

    Change currency information

--- app/helpers/exchanges_helper.rb
+++ app/helpers/exchanges_helper.rb
@@ -1,10 +1,20 @@
 module ExchangesHelper
-    def currency_list
+  def currency_list
+    [
+      ['FAVORITOS',
+        [['REAL BRASIL','BRL'], ['EURO','EUR'], ['DOLAR DOS EUA','USD']]
+      ],
+      ['OUTROS',
         [
-          'AUD', 'BGN', 'BRL', 'CAD', 'CHF', 'CNY', 'CZK', 'DKK', 'EUR', 'GBP',
-          'HKD', 'HRK', 'HUF', 'IDR', 'ILS', 'INR', 'JPY', 'KRW', 'MXN',
-          'MYR', 'NOK', 'NZD', 'PHP', 'PLN', 'RON', 'RUB', 'SEK', 'SGD',
-          'THB', 'TRY', 'USD', 'ZAR'
+          ['DOLAR AUSTRALIANO','AUD'], ['LEV BULGARIA','BGN'], ['DOLAR CANADENSE','CAD'], ['FRANCO SUICO','CHF'],
+          ['RENMINBI CHINES','CNY'], ['COROA TCHECA','CZK'], ['COROA DINAMARQUESA','DKK'], ['LIBRA ESTERLINA','GBP'],
+          ['DOLAR HONG KONG','HKD'], ['KUNA CROATA','HRK'], ['FORINT HÚNGARO','HUF'], ['RUPIA INDONÉSIA]','IDR'],
+          ['SHEKEL ISRAEL','ILS'], ['RUPIA INDIA','INR'], ['IENE JAPÃO','JPY'], ['WON COREIA SUL','KRW'],
+          ['PESO MEXINO','MXN'],['RINGGIT MALAIO','MYR'], ['COROA NORUEGUESA','NOK'], ['DOLAR NEOZELANDES','NZD'], 
+          ['PESO FILIPINO','PHP'], ['ZLOTI POLONÊS','PLN'], ['LEU ROMENO','RON'], ['RUBLO RUSSO','RUB'], ['COROA SUECA','SEK'],
+          ['DOLAR CINGAPURA','SGD'], ['BATH TAILANDIA','THB'], ['LIRA TURCA','TRY'], ['RAND SUL AFRICANO','ZAR']
         ]
-      end
+      ]
+    ]
+  end
 end
