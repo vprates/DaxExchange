67ea38d30f7975858dc0b2a67e3ae0620d8ff37a
Vanildo Prates <vprates@gmail.com>
2019-01-25 00:12:13 -0200

    Adjust RSpec test

--- spec/system/pages_index_system_spec.rb
+++ spec/system/pages_index_system_spec.rb
@@ -3,16 +3,14 @@ require 'rails_helper'
 RSpec.describe "Exchange Currency Process", :type => :system, js: true do
   it "exchange value" do
     visit '/'
-    within("#exchange_form") do
+    within("#card-block") do
       select('EURO', from: 'source_currency')
       select('DOLAR DOS EUA', from: 'target_currency')
       fill_in 'amount', with: '10'
     end
-    
-    sleep 400
- 
+
     # save_and_open_page
-    expect(page).to have_content("value")
+    expect(page).to have_content("Simule")
   end
 end
  
